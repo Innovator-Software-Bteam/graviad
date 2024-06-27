280c28e7deabf86ed31cd1c6a558f6d5c4677679
HoangDucBach <hoangbach0985@gmail.com>
2024-06-27 20:47:04 +0700

    Deploy

--- srcs/server-ver2/src/config/database.config.ts
+++ srcs/server-ver2/src/config/database.config.ts
@@ -8,6 +8,7 @@ export default registerAs('database', (): TypeOrmModuleOptions => ({
     username: process.env.GRAVIAD_DATABASE_USERNAME,
     password: process.env.GRAVIAD_DATABASE_PASSWORD,
     database: process.env.GRAVIAD_DATABASE_NAME,
+    ssl: process.env.GRAVIAD_DATABASE_SSL === 'true',
 
     type: process.env.GRAVIAD_DATABASE_ORM_TYPE as any,
     synchronize: process.env.GRAVIAD_DATABASE_ORM_SYNCHRONIZE === 'true',
