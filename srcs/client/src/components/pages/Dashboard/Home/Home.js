0d10f62adccc5627c93c7e8ed9c6b91ab6e55eeb
HoangDucBach <hoangbach0985@gmail.com>
2024-06-28 06:53:42 +0700

    Deploy

--- srcs/client/src/components/pages/Dashboard/Home/Home.js
+++
@@ -1,7 +0,0 @@
-export function HomeDashboard() {
-    return (
-        <div className="Home">
-            <h1>HomeDashboard</h1>
-        </div>
-    );
-}
--- srcs/client/src/routers/DashboardRouter.js
+++
@@ -1,9 +0,0 @@
-import Dashboard from "../Dashboard";
-import {Route} from "react-router-dom";
-import {RequiredAuth} from "../protectedComponent/auth/RequiredAuth";
-
-export const DashboardRouter =
-    <Route path={'dashboard'} element={
-        <RequiredAuth component={Dashboard}/>
-    }>
-    </Route>
