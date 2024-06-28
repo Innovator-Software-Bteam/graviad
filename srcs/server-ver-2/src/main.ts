faa0cd7e69b8767ff04be259e794d406f77109df
HoangDucBach <hoangbach0985@gmail.com>
2024-06-28 09:17:44 +0700

    Change cors

--- srcs/server-ver-2/src/main.ts
+++ srcs/server-ver-2/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 
     app.use(cookieParser());
     app.use(cors({
-        origin: 'https://graviad.vercel.app',
+        origin: ['https://graviad.vercel.app', 'http://localhost:3000'],
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
         allowedHeaders: ['Content-Type', 'Authorization'],
